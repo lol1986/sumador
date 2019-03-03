d579ade63676abcee206c593b0d4c45fe40fe4eb
Laura Ortiz <lauraortizluque@gmail.com>
2019-03-03 21:50:30 +0100

    test una cifra ok

--- src/sumadorLOL/ASumar.java
+++ src/sumadorLOL/ASumar.java
@@ -1,14 +1,43 @@
 package sumadorLOL;
-
+/**
+ * Asumar es una clase creada para sumar los dígitos de un número entero
+ * Está formada por un entero, y tiene los métodos para dividir este entero 
+ * en dígitos y realizar la suma de los mismos. 
+ * @author Laura Ortiz Luque
+ * @version 1.0
+ *
+ */
 public class ASumar {
 
-	public ASumar(String numero) {
-		// TODO Auto-generated constructor stub
+	private String valor_inicial;
+	
+	/**
+	 * ASumar es el constructor de la clase. 
+	 * @param valor_inicial es el número entero del que queremos sumar los dígitos
+	 * 
+	 */
+	public ASumar(String valor_inicial) {
+		this.valor_inicial=valor_inicial;
 	}
 
+	 /**
+	  * mostrar imprime el resultado de la suma
+	  * 
+	  * @return	la capacidad (en litros) del depÃ³sito
+	  * */
 	public String mostrar() {
-		// TODO Auto-generated method stub
-		return null;
+		int i=0;
+		String resultado="";
+		int total=0;
+		char[] numeros=this.valor_inicial.toCharArray();
+		for (i=0;i<this.valor_inicial.length();i++) {
+			if (i>0) {
+				resultado= resultado +" + ";
+			}
+			total=total+Character.getNumericValue(numeros[i]);
+			resultado= resultado+numeros[i];	
+		}
+		resultado = resultado +" = "+total;
+		return resultado;
 	}
-
 }
