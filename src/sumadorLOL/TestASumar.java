99fa102e7e9fdd7ebe4eb0d7dafc07d3363b7181
Laura Ortiz <lauraortizluque@gmail.com>
2019-03-03 21:57:43 +0100

    test prueba cifra negativa (falla)

--- src/sumadorLOL/TestASumar.java
+++ src/sumadorLOL/TestASumar.java
@@ -1,16 +1,35 @@
 package sumadorLOL;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+/**
+ * ASumar es el constructor de la clase. 
+ * @param valor_inicial es el número entero del que queremos sumar los dígitos
+ * 
+ */
 public class TestASumar {
 
+	/**
+	 * mostrar imprime el resultado de la suma
+	 * 
+	 * @return	la capacidad (en litros) del depÃ³sito
+	 * */
 	@Test
 	public void testUnaCifra() {
 		ASumar sumi = new ASumar("5");
 		String resultado = sumi.mostrar();
 		assertEquals("5 = 5",resultado );
-		}
+	}
+
+	/**
+	 * Método de prueba para cifra negativa
+	 */
+	@Test
+	public void testCifraNegativa() {
+		ASumar sumi = new ASumar("-3433");
+		String resultado = sumi.mostrar();
+		assertEquals("",resultado );
+	}
 
 }
