eaeeac86c2a84bc0de912c8b94ea77c4b1d48ea1
Laura Ortiz <lauraortizluque@gmail.com>
2019-03-03 21:36:42 +0100

    Versión inicial

---
+++ src/sumadorLOL/ASumar.java
@@ -0,0 +1,5 @@
+package sumadorLOL;
+
+public class ASumar {
+
+}
---
+++ src/sumadorLOL/TestASumar.java
@@ -0,0 +1,14 @@
+package sumadorLOL;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestASumar {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
